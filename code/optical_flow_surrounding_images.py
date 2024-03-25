0fbf81ac292709e05d37407776f2d6d0f01e81bb
BenMaslen <ben.maslen95@gmail.com>
2024-03-25 16:09:03 +1100

    cleaned some code

--- code/optical_flow_surrounding_images.py
+++ code/optical_flow_surrounding_images.py
@@ -134,12 +134,12 @@ for im_num in im_num_list:
 
     flow1 = cv2.calcOpticalFlowFarneback(prvs,middle, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-    mag, ang = cv2.cartToPolar(flow1[...,0], flow1[...,1])
-    hsv = np.zeros_like(np.array(convert_image(img_center_tens)))
+    mag, ang   = cv2.cartToPolar(flow1[...,0], flow1[...,1])
+    hsv        = np.zeros_like(np.array(convert_image(img_center_tens)))
     hsv[...,1] = 255
     hsv[...,0] = ang*180/np.pi/2
     hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    flow_img = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    flow_img   = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
 
     hsv_tens = convert_tensor(hsv)
 
